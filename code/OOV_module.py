59310d58f61101f206e29a8194a820810285edd5
Florent Rambaud <flo.rambaud@gmail.com>
2020-02-28 15:12:10 +0100

    code folder

---
+++ code/OOV_module.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 15:04:38 2020
+
+@author: flo-r
+"""
+
+import os
+import numpy as np
+
+os.chdir('C:/Users/flo-r/Desktop/Cours MVA/S2/speech_nlp/probabilistic_french_parser')
